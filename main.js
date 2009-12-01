b15b14dd058fcb9d99f6bee50f0c481c0a96a4b2
liquidz <liquidz.uo@gmail.com>
2009-12-01 20:37:37 +0900

    add text type

--- main.js
+++ main.js
@@ -1,11 +1,20 @@
 var taskpaper = {};
-taskpaper.appName = "unitaskpapera";
-taskpaper.defaultPage = "index";
-taskpaper.page = taskpaper.defaultPage;
-taskpaper.defaultContent = "- sample\n- done task #done";
+taskpaper.constant = {
+	APP_NAME: "unitaskpapera",
+	DEFAULT_PAGE: "index",
+	DEFAULT_CONTENT: "- sample\n- done task #done",
+	TYPE_TASK: 0,
+	TYPE_GROUP: 1,
+	TYPE_TEXT: 2
+};
+//taskpaper.appName = "unitaskpapera";
+//taskpaper.defaultPage = "index";
+taskpaper.page = taskpaper.constant.DEFAULT_PAGE;
+//taskpaper.defaultContent = "- sample\n- done task #done";
 
 taskpaper.makeKeyName = function(){
-	return this.appName + "_" + this.page;
+	//return this.appName + "_" + this.page;
+	return this.constant.APP_NAME + "_" + this.page;
 };
 
 taskpaper.parseLine = function(line){
@@ -14,30 +23,40 @@ taskpaper.parseLine = function(line){
 	var done = false;
 
 	if(text.indexOf(":") === text.length - 1){
+		// group
 		return {
-			isTask: false,
+			type: this.constant.TYPE_GROUP,
 			caption: text
 		};
 	} else {
-		var index = text.indexOf("#");
-		if(index !== -1){
-			tags = mouf.trim(text.substr(index)).split(/\s+/)
-			text = text.substr(0, index);
-			if(tags){
-				mouf.each(tags, function(){
-					if(this.indexOf("#done") !== -1){
-						done = true;
-						return true;
-					}
-				});
+		if(text.indexOf("-") === 0){
+			// task
+			var index = text.indexOf("#");
+			if(index !== -1){
+				tags = mouf.trim(text.substr(index)).split(/\s+/)
+				text = text.substr(0, index);
+				if(tags){
+					mouf.each(tags, function(){
+						if(this.indexOf("#done") !== -1){
+							done = true;
+							return true;
+						}
+					});
+				}
 			}
+			return {
+				type: this.constant.TYPE_TASK,
+				caption: text,
+				done: done,
+				tags: tags
+			};
+		} else {
+			// text
+			return {
+				type: this.constant.TYPE_TEXT,
+				caption: text
+			};
 		}
-		return {
-			isTask: true,
-			caption: text,
-			done: done,
-			tags: tags
-		};
 	}
 };
 
@@ -47,6 +66,7 @@ taskpaper.parse = function(str){
 		tasks: {},
 		hasTask: false
 	};
+	var task = null;
 	var res = {};
 	var count = {
 		group: 0,
@@ -56,17 +76,27 @@ taskpaper.parse = function(str){
 	mouf.each(str.split(/[\r\n]+/), function(){
 		var parsedItem = taskpaper.parseLine(this);
 
-		if(parsedItem.isTask){
+		//if(parsedItem.isTask){
+		if(parsedItem.type === taskpaper.constant.TYPE_TASK){
 			// task
 			group.tasks[count.task++] = parsedItem;
 			group.hasTask = true;
-		} else {
+		} else if(parsedItem.type === taskpaper.constant.TYPE_GROUP){
 			// group
 			if(group.hasTask) res[count.group++] = mouf.clone(group);
 
 			group.name = parsedItem.caption;
 			group.tasks = {};
 			group.hasTask = false;
+		} else if(parsedItem.type === taskpaper.constant.TYPE_TEXT){
+			// text
+			var index = count.task - 1;
+			if(group.tasks[index].text){
+				group.tasks[index].text += parsedItem.caption;
+			} else {
+				//group.tasks[index].text = "<p>" + parsedItem.caption + "</p>";
+				group.tasks[index].text = parsedItem.caption;
+			}
 		}
 	});
 	res[count.group] = group;
@@ -75,7 +105,8 @@ taskpaper.parse = function(str){
 
 (function(){
 	var getPageName = function(req){
-		var pageName = taskpaper.defaultPage;
+		//var pageName = taskpaper.defaultPage;
+		var pageName = taskpaper.constant.DEFAULT_PAGE;
 		var path = req.uri.split("/");
 		if(path.length > 3){
 			var index = path.length - 1;
@@ -94,8 +125,10 @@ taskpaper.parse = function(str){
 		return mouf.render("template/view.htm", {
 			page: taskpaper.page,
 			owner: conn.isOwner,
-			items: taskpaper.parse(mouf.get(taskpaper.makeKeyName(), taskpaper.defaultContent)),
-			isIndex: (taskpaper.page === taskpaper.defaultPage),
+			//items: taskpaper.parse(mouf.get(taskpaper.makeKeyName(), taskpaper.defaultContent)),
+			items: taskpaper.parse(mouf.get(taskpaper.makeKeyName(), taskpaper.constant.DEFAULT_CONTENT)),
+			//isIndex: (taskpaper.page === taskpaper.defaultPage),
+			isIndex: (taskpaper.page === taskpaper.constant.DEFAULT_PAGE),
 			root: mouf.getAddress(req)
 		});
 	};
@@ -109,7 +142,8 @@ taskpaper.parse = function(str){
 			return mouf.render("template/edit.htm", {
 				page: taskpaper.page,
 				root: mouf.getAddress(req),
-				data: mouf.get(taskpaper.makeKeyName(), taskpaper.defaultContent)
+				//data: mouf.get(taskpaper.makeKeyName(), taskpaper.defaultContent)
+				data: mouf.get(taskpaper.makeKeyName(), taskpaper.constant.DEFAULT_CONTENT)
 			});
 		} else {
 			return mouf.render("template/error.htm", {});
@@ -124,7 +158,8 @@ taskpaper.parse = function(str){
 			var page = req.bodyItems.page[0];
 
 			taskpaper.page = page;
-			var content = mouf.get(taskpaper.makeKeyName(), taskpaper.defaultContent);
+			//var content = mouf.get(taskpaper.makeKeyName(), taskpaper.defaultContent);
+			var content = mouf.get(taskpaper.makeKeyName(), taskpaper.constant.DEFAULT_CONTENT);
 			var newContent = [];
 			var newLine = null;
 			mouf.each(content.split(/[\r\n]+/), function(index){
